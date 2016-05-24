5568b1e1e067ce3fe1979a6cb456408a967d28dc
Florent Michiel <Florent Michiel>
2016-05-24 21:10:48 +0200

    binouze

--- TP2/js/script.js
+++ TP2/js/script.js
@@ -1,12 +1,12 @@
 function initialize() {
-	map = new google.maps.Map(document.getElementById("map_binouze"){
+	map = new google.maps.Map(document.getElementById("map_canvas"){
 		zoom: 19,
         center: new google.maps.LatLng(48.858565, 2.347198),
         mapTypeId: google.maps.MapTypeId.ROADMAP
      });   
 } 
  
-if (navigator.geolocation){
+if(navigator.geolocation){
 	var watchId = navigator.geolocation.watchPosition(successCallback, null, {enableHighAccuracy:true});
 }
 else{
