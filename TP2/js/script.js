4b2b751c665779e5962c1d43cfec80fca98d700f
Florent Michiel <Florent Michiel>
2016-05-25 10:45:37 +0200

    binouze

--- TP2/js/script.js
+++ TP2/js/script.js
@@ -1,5 +1,5 @@
 function initialize() {
-	map = new google.maps.Map(document.getElementById("map_canvas"){
+	map = new google.maps.Map(document.getElementById("map_canvas"),{
 		zoom: 19,
         center: new google.maps.LatLng(48.858565, 2.347198),
         mapTypeId: google.maps.MapTypeId.ROADMAP
